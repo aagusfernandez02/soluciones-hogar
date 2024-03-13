d1b544aba053e14175c37d4ee5e0312e0034b575
Agustín Fernandez <af.agusfernandez02@gmail.com>
2024-03-13 19:26:39 -0300

    cambio numero de telefono

--- src/components/ContactSection/ContactSection.jsx
+++ src/components/ContactSection/ContactSection.jsx
@@ -3,7 +3,7 @@ import styles from './ContactSection.module.css';
 
 const ContactForm = () => {
 
-  let link_whatsapp = 'https://api.whatsapp.com/send?phone=541154905706&text=Hola%20Guillermo%2C%20%C2%BFc%C3%B3mo%20est%C3%A1s%3F%0ATe%20contacto%20a%20trav%C3%A9s%20de%20la%20p%C3%A1gina%20web%20y%20me%20gustar%C3%ADa%20que%20te%20comuniques%20conmigo!';
+  let link_whatsapp = 'https://api.whatsapp.com/send?phone=541165854725&text=Hola%20Guillermo%2C%20%C2%BFc%C3%B3mo%20est%C3%A1s%3F%0ATe%20contacto%20a%20trav%C3%A9s%20de%20la%20p%C3%A1gina%20web%20y%20me%20gustar%C3%ADa%20que%20te%20comuniques%20conmigo!';
 
 
   return (
